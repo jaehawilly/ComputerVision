53bbd78b3481093a5339e2b0ae840dc6bf363349
jaehawilly <jaehawilly@naver.com>
2022-09-16 13:13:50 +0900

    Initial Commit

---
+++ hw1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by 이재하 on 2022/09/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
